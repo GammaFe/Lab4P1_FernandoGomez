58fa7568b987ff9d5acd64b15cc2be4431060234
GammaFe <115840414+GammaFe@users.noreply.github.com>
2022-11-04 17:21:46 -0600

    Add files via upload

---
+++ Lab4P1_FernandoGomez.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package com.mycompany.lab4p1_fernandogomez;
+
+/**
+ *
+ * @author Fernando_Jose
+ */
+public class Lab4P1_FernandoGomez {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
